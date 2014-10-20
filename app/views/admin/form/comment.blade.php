d883b18bcd61f17b15124fb38bc1aa64bebec499
Sleeping Owl <owl.sleeping@yahoo.com>
2014-10-20 14:25:33 +0400

    Fixed mistyped word

--- app/views/admin/form/comment.blade.php
+++ app/views/admin/form/comment.blade.php
@@ -1,3 +1,3 @@
 <p class="help-block">
-	<strong>demo limitation:</strong> CKEditor image upload function is disabled for this demo. In your application in will be enabled by default.
+	<strong>demo limitation:</strong> CKEditor image upload function is disabled for this demo. In your application it will be enabled by default.
 </p>
