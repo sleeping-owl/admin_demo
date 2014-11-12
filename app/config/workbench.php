81cce4edcce0f564b03f7d5521cb56b1c4c9a141
Sleeping Owl <owl.sleeping@yahoo.com>
2014-11-12 19:12:16 +0400

    Workbench credentials updated

--- app/config/workbench.php
+++ app/config/workbench.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'name' => '',
+	'name' => 'Sleeping Owl',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -26,6 +26,6 @@ return array(
 	|
 	*/
 
-	'email' => '',
+	'email' => 'owl.sleeping@yahoo.com',
 
 );
