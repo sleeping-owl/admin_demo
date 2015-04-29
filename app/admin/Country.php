a1834f509709ecb9c8149d731a52e1787e7b4022
Sleeping Owl <owl.sleeping@yahoo.com>
2015-04-29 12:57:06 +0400

    Sortable demo rollback

--- app/admin/Country.php
+++ app/admin/Country.php
@@ -2,7 +2,6 @@
 
 Admin::model('\Country')->title('Countries')->with('contacts')->columns(function ()
 {
-	Column::string('id', 'ID')->sortableDefault('desc');
 	Column::string('title', 'Title');
 	Column::count('contacts', 'Contacts')->append(Column::filter('country_id')->model('\Contact'));
 })->form(function ()
